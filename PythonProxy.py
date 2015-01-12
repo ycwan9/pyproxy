8f3a1abd57201908cf58f8c352c185f3df6a613f
ycwan9 <ycwangqwerty@126.com>
2015-01-12 12:29:10 +0800

    toHex()

--- PythonProxy.py
+++ PythonProxy.py
@@ -11,6 +11,15 @@ BUFLEN = 8192
 VERSION = 'Python Proxy/'+__version__
 HTTPVER = 'HTTP/1.1'
 
+def toHex(s):
+    lst = []
+    for ch in s:
+        hv = hex(ord(ch)).replace('0x', '')
+        if len(hv) == 1:
+            hv = '0'+hv
+        lst.append(hv)
+    return reduce(lambda x,y:x+y, lst)
+
 class socketHandler(SocketServer.StreamRequestHandler):
     def handle(self):
         print 'handle'
@@ -98,5 +107,3 @@ if __name__ == '__main__':
     HOST, PORT = "", 8080
     server = SocketServer.TCPServer((HOST, PORT), socketHandler)
     server.serve_forever()
-
-
